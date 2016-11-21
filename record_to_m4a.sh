667d11543bb1c36a919a210c7fa420dd4f224012
ojeme <ojeme@mojjo.fr>
2016-11-21 11:17:39 +0100

    added audio recording scripts

---
+++ record_to_m4a.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+if [ -z $1 ];
+then
+	FILE="$(date -Is)"
+	FILE=${FILE::-5}
+	FILE=$(echo $FILE | sed "s/T/_/")
+	FILE=$(echo $FILE | sed "s/:/h/")
+	FILE=$(echo $FILE | sed "s/:/min/")
+	FILE=$(echo $FILE | sed "s/+/s/")
+	FILE="$FILE.m4a"
+else
+	FILE="$1"
+fi
+
+#echo file : $FILE
+
+ffmpeg -f alsa -i pulse -c:a libfdk_aac -b:a 128k "$FILE"
