9d6d896e1282d4f7853abdb1470033c4a47e3823
mejdo <ikhalo.o@hotmail.fr>
2017-10-23 23:48:37 +0200

    feat(rename_episode): handle 101 counting + srt and video files together

--- rename_episode.sh
+++ rename_episode.sh
@@ -10,46 +10,79 @@
 echo "Enter the name of the serie you want to rename"
 read serie
 apply=false
+video_search=0
 
 while true
 do
-  for season in {00..29}
-  do
-    for ep in {00..29}
+    for season in {01..20}
     do
-      bad_file=`ls -1| egrep -i -m 1 "[[:print:]]*${serie}[[:print:]]+${season}(e|x)${ep}[[:print:]]*\.(srt|mkv|flv|avi|mp4)$" 2> /dev/null`
-      extension=`echo $bad_file |tail -c 4`
-      file_name="${serie} ${season}x${ep}.${extension}"
-  
-      #echo file_name : $file_name
-      #echo bad_file : $bad_file
-    
-      if [ -e "$bad_file" ]
-      then
-        if [[ $apply == true ]]
+        if [ $season -lt 10 ]
         then
-          mv "$bad_file" "$file_name"
+            season_unit=`echo $season| tail -c 2`
+            season_hundred=$((season_unit * 100))
         else
-          echo "$bad_file --> $file_name"
+            season_hundred=$((season * 100))
         fi
-      fi
-    done
-  done
 
-  if [[ $apply != true ]]
-  then
-    echo "Do you wish to apply the changes ? [y/N]"
-    read apply
-    if [[ $apply == y ]] || [[ $apply == Y ]]
+        for ep in {00..29}
+        do
+            count=0
+            if [ $ep -lt 10 ]
+            then
+                ep_unit=`echo $ep| tail -c 2`
+                season_ep=$((season_hundred + ep_unit)) 
+            else
+                season_ep=$((season_hundred + ep)) 
+            fi
+
+            while [ $count -lt 2 ]
+            do
+                count=$(( count + 1 ))
+                if [ $video_search -eq 0 ]
+                then
+                    bad_file=`ls -1| egrep -i -m 1 "[[:print:]]*${serie}[[:print:]]+($season_ep[^p]|${season}(e|x)${ep})[[:print:]]*\.(srt)$" 2> /dev/null`
+                else
+                    bad_file=`ls -1| egrep -i -m 1 "[[:print:]]*${serie}[[:print:]]+($season_ep[^p]|${season}(e|x)${ep})[[:print:]]*\.(mkv|flv|avi|mp4)$" 2> /dev/null`
+                fi
+                video_search=$((!$video_search))
+
+                extension=`echo $bad_file |tail -c 4`
+                file_name="${serie} ${season}x${ep}.${extension}"
+
+                #echo file_name : $file_name
+                #echo bad_file : $bad_file
+
+                if [ -e "$bad_file" ]
+                then
+                    if [[ $bad_file == $file_name ]]
+                    then
+                        continue
+                    fi
+                    if [[ $apply == true ]]
+                    then
+                        mv "$bad_file" "$file_name"
+                    else
+                        echo "$bad_file --> $file_name"
+                    fi
+                fi
+            done
+        done
+    done
+
+    if [[ $apply != true ]]
     then
-        apply=true
+        echo "Do you wish to apply the changes ? [y/N]"
+        read apply
+        if [[ $apply == y ]] || [[ $apply == Y ]]
+        then
+            apply=true
+        else
+            echo -e "The changes won't be applied.\nExiting."
+            exit 0
+        fi
     else
-        echo -e "The changes won't be applied.\nExiting."
+        echo -e "Changed applied.\nExiting."
         exit 0
     fi
-  else
-    echo -e "Changed applied.\nExiting."
-    exit 0
-  fi
 
 done
