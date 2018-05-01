3ebf849e6c4f7a582f291d70d21dc16675a63b06
medjo <ikhalo.o@hotmail.fr>
2018-05-01 17:29:51 +0200

    feat: add history log with first entry is the newest

--- mv_seen_episodes.sh
+++ mv_seen_episodes.sh
@@ -5,9 +5,22 @@
 # remote raspberry pi
 
 FILETYPES="*.srt *.flv *.mp4 *.avi *.mkv"
-DEST="pi-famille:~/Videos/Series/"
+REMOTE_HOST="pi-famille"
+DEST="$REMOTE_HOST:~/Videos/Series/"
+#SRC="$HOME/test/rsync/"
 SRC="$HOME/Vidéos/Séries/Vu"
 OPT="-vh --ignore-missing-args --progress --remove-source-files"
+TRANSFERT_LOG="~/Videos/Series/transfert.log"
+TMP_LOG="~/Videos/Series/transfert.tmp.log"
+TMP_LOG2="~/Videos/Series/transfert.tmp.log.2"
 
 cd $SRC
-rsync $OPT $FILETYPES $DEST
+# Do the transfert and write output to temporary log
+echo -e "\t\t $(date)" | ssh $REMOTE_HOST "cat >> $TMP_LOG"
+rsync $OPT $FILETYPES $DEST | ssh $REMOTE_HOST "cat >> $TMP_LOG"
+echo -e "\n\t\t-------------------------------------------\n" |
+    ssh $REMOTE_HOST "cat >> $TMP_LOG"
+
+# Append temporary log to beginning of log and remove temporary files
+ssh $REMOTE_HOST "cat $TMP_LOG $TRANSFERT_LOG > $TMP_LOG2 2>/dev/null;\
+    mv $TMP_LOG2 $TRANSFERT_LOG; rm -f $TMP_LOG"
